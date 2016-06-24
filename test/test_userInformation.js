4b6b348ff9a331575b4155a980f31d85486f5484
kevin elberger <kevin elberger>
2016-06-24 11:07:32 -0700

    Add validations for user information test cases

--- test/test_userInformation.js
+++ test/test_userInformation.js
@@ -9,24 +9,32 @@ describe('UserInformation', function() {
        assert.isNull(testUser.requested_information);
     });
 
-    it('should have a username', function() {
+    it('should have a valid username', function() {
         assert.isNotNull(testUser.username);
+        assert.isAbove(testUser.username.length, 0);
     });
 
-    it('should have a level', function() {
+    it('should have a valid level', function() {
         assert.isNotNull(testUser.level);
+        assert.isNumber(testUser.level);
+        assert.isAtLeast(testUser.level, 1);
     });
 
-    it('should have a title', function() {
+    it('should have a valid title', function() {
         assert.isNotNull(testUser.title);
+        assert.isAbove(testUser.title, 0);
     });
 
     it('should have topics_count', function() {
         assert.isNotNull(testUser.topics_count);
+        assert.isNumber(testUser.topics_count);
+        assert.isAtLeast(testUser.topics_count, 0);
     });
 
     it('should have posts_count', function() {
         assert.isNotNull(testUser.posts_count);
+        assert.isNumber(testUser.posts_count);
+        assert.isAtLeast(testUser.posts_count, 0);
     });
 
     it('should have a creation_date', function() {
