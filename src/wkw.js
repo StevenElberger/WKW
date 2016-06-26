517f22a8e6de2f7392f4fee5d46f028416a92043
Steven Elberger <stevenelberger@gmail.com>
2016-06-26 11:18:19 -0700

    Refactors User constructor -Extracts getUserInformation and getStudyQueue -Replaces $.ajax with $.getJSON

--- src/wkw.js
+++ src/wkw.js
@@ -1,7 +1,8 @@
 // WKW - Client Side JS Wrapper for WaniKani API
 
 
-// User Information (user.user_information)
+// User Information Prototype (user.user_information)
+// @time - time limit before data is expired
 // @expiration (Number) - unix timestamp for last refresh
 // @username (String) - username
 // @gravatar (String) - md5 gravatar
@@ -15,11 +16,13 @@
 // @creation_date (Number) - unix timestamp for account creation
 // @vacation_date (Number or null) - unix timestamp for vacation setting
 var UserInformation = {
+    "time": 21600000,
     "expiration": new Date(),
-    isExpired() { return new Date() - this.expiration > 10000; }
+    isExpired() { return new Date() - this.expiration > this.time; }
 };
 
-// Study Queue (user.study_queue)
+// Study Queue Prototype (user.study_queue)
+// @time - time limit before data is expired
 // @expiration (Number) - unix timestamp for last refresh
 // @lessons_available (Number) - number of lessons currently available
 // @reviews_available (Number) - number of reviews currently available
@@ -27,9 +30,36 @@ var UserInformation = {
 // @reviews_available_next_hour (Number) - number of reviews available within the next hour
 // @reviews_available_next_day (Number) - number of reviews available within the next day
 var StudyQueue = {
+    "time": 900000,
     "expiration": new Date(),
-    isExpired() { return new Date() - this.expiration > 10000; }
+    isExpired() { return new Date() - this.expiration > this.time; }
 };
+
+// Retrieves the user information.
+// @param callback (fn) - callback function
+var getUserInformation = function(callback) {
+    if (!this.user_information.isExpired() && this.user_information.username != null) { callback(); }
+    var that = this;
+    var wk_url = "https://www.wanikani.com/api/user/" + this.key;
+    $.getJSON(wk_url, function(data) {
+        for (var d in data.user_information)
+            that.user_information[d] = data.user_information[d];
+        callback();
+    });
+};
+
+// Retrieves the user's study queue.
+// @param callback (fn) - callback function
+var getStudyQueue = function(callback) {
+    if (!this.study_queue.isExpired() && this.study_queue.lessons_available != null) { callback(); }
+    var that = this;
+    var wk_url = "https://www.wanikani.com/api/user/" + this.key + "/study-queue";
+    $.getJSON(wk_url, function(data) {
+        for (var d in data.requested_information)
+            that.study_queue[d] = data.requested_information[d];
+        callback();
+    });
+}
 
 // Constructor for user objects.
 // @key (Number) - user's WK API key
@@ -38,48 +68,10 @@ var StudyQueue = {
 function User(api_key) {
     var resultUser = {
         key: api_key,
-        getUserInformation(callback) {
-            if (!this.user_information.isExpired() && this.user_information.username != null) { callback(); }
-            var that = this;
-            var wk_url = "https://www.wanikani.com/api/user/";
-            var request = $.ajax({
-                url: wk_url + this.key,
-                type: "GET",
-            });
-            request.done(function(data) {
-                that.user_information.username = data.user_information.username;
-                that.user_information.gravatar = data.user_information.gravatar;
-                that.user_information.level = data.user_information.level;
-                that.user_information.title = data.user_information.title;
-                that.user_information.about = data.user_information.about;
-                that.user_information.website = data.user_information.website;
-                that.user_information.twitter = data.user_information.twitter;
-                that.user_information.topics_count = data.user_information.topics_count;
-                that.user_information.posts_count = data.user_information.posts_count;
-                that.user_information.creation_date = data.user_information.creation_date;
-                that.user_information.vacation_date = data.user_information.vacation_date;
-                callback();
-            });
-        },
-        getStudyQueue(callback) {
-            if (!this.study_queue.isExpired() && this.study_queue.lessons_available != null) { callback(); }
-            var that = this;
-            var wk_url = "https://www.wanikani.com/api/user/";
-            var request = $.ajax({
-                url: wk_url + this.key + "/study-queue",
-                type: "GET",
-            });
-            request.done(function(data) {
-                that.study_queue.lessons_available = data.requested_information.lessons_available;
-                that.study_queue.reviews_available = data.requested_information.reviews_available;
-                that.study_queue.next_review_date = data.requested_information.next_review_date;
-                that.study_queue.reviews_available_next_hour = data.requested_information.reviews_available_next_hour;
-                that.study_queue.reviews_available_next_day = data.requested_information.reviews_available_next_day;
-                callback();
-            });
-        }
     };
     resultUser.user_information = new Object(UserInformation);
     resultUser.study_queue = new Object(StudyQueue);
+    resultUser.getUserInformation = getUserInformation;
+    resultUser.getStudyQueue = getStudyQueue;
     return resultUser;
 }
